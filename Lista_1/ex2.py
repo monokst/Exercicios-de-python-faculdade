f7d1c7d133121746ad27b8fafbe0e2b6d9a98e31
Mariana Leão <118079484+monokst@users.noreply.github.com>
2026-08-15 14:53:29 -0300

    Update ex2.py

---
+++ Lista_1/ex2.py
@@ -0,0 +1,5 @@
+#Exercicio 2 | Soma de dois valores
+valor = int(input("Insira um valor: ")) #o usuario vai inserir o valor
+valor2 = int(input("Insira um valor: "))
+
+print(f"a soma do valores {valor + valor2}") #O f permite colocar variáveis e contas dentro de um texto usando {}
